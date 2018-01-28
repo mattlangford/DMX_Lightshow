2c5fa289f494f4c179e9e028cfd24928c7d65aa7
Matt Langford <matthew.langford95@gmail.com>
2018-01-27 19:44:49 -0500

    Add more FFT helpers to get frequencies in a range

--- fft_helpers.hh
+++ fft_helpers.hh
@@ -17,12 +17,45 @@ public: // types //////////////////////////////////////////////////////////////
 typedef std::complex<T> ComplexT;
 typedef std::valarray<ComplexT> CArray;
 
+//
+// A single frequency bin
+//
+struct FrequencyBin
+{
+    double frequency; // hz
+    double amplitude; // dB? Not sure
+};
+
 public: // methods ////////////////////////////////////////////////////////////
 
 //
-// Compute FFT on time domain sample non-complex numbers
+// Get the index in the bins array that corresponds to a particular frequency
 //
-static std::vector<T> compute_fft(const boost::circular_buffer<T>& time_domain_samples)
+static inline size_t index_from_frequency(const double frequency, const std::vector<FrequencyBin>& bins)
+{
+    const double frequency_factor = double(bins.size()) / SAMPLE_RATE;
+    return static_cast<size_t>(frequency * frequency_factor);
+}
+
+//
+// Get the frequency that corresponds to a particular index
+//
+static inline double frequency_from_index(const size_t index, const std::vector<FrequencyBin>& bins)
+{
+    if (bins.size() == 0)
+    {
+        throw "div by zero!";
+    }
+
+    const double frequency_factor = double(SAMPLE_RATE) / bins.size();
+    return index * frequency_factor;
+}
+
+//
+// Compute FFT on time domain sample non-complex number. I'm using a circular buffer since that's
+// how the data will be formatted
+//
+static std::vector<FrequencyBin> compute_fft(const boost::circular_buffer<T>& time_domain_samples)
 {
     //
     // Take the raw float data and create a complex array with it
@@ -40,10 +73,10 @@ static std::vector<T> compute_fft(const boost::circular_buffer<T>& time_domain_s
 //
 // Save some compute, if you've already got a complex vector
 //
-static std::vector<T> compute_fft(const std::vector<ComplexT>& time_domain_samples)
+static std::vector<FrequencyBin> compute_fft(const std::vector<ComplexT>& time_domain_samples)
 {
     //
-    // Compute the FFT over that data
+    // Compute the FFT over the complex data
     //
     CArray transformed_complex_data{time_domain_samples.data(), time_domain_samples.size()};
     fft(transformed_complex_data);
@@ -52,21 +85,40 @@ static std::vector<T> compute_fft(const std::vector<ComplexT>& time_domain_sampl
     // Convert back from complex to just the magnitudes of the complex numbers
     //
     const size_t frequency_bins = transformed_complex_data.size() / 2;
-    std::vector<T> magnitudes;
-    magnitudes.reserve(frequency_bins);
+
+    std::vector<FrequencyBin> bins;
+    bins.resize(frequency_bins);
     for (size_t i = 0; i < frequency_bins; ++i)
     {
-        magnitudes.push_back(std::norm(transformed_complex_data[i]));
+        bins[i].frequency = frequency_from_index(i, bins);
+        bins[i].amplitude = std::norm(transformed_complex_data[i]) / frequency_bins;
     }
+
+    return bins;
 }
 
 //
-// Given an index from a vector that was compute with compute_fft, generate
+// Generate a map: frequency->magnitude in some frequency range
 //
-static inline double get_frequency_from_index(const std::vector<T>& fft_data, const size_t i)
+static std::vector<FrequencyBin> get_frequencies_in_range(const double frequency_min,
+                                                          const double frequency_max,
+                                                          const std::vector<T>& fft_data)
 {
-    const double frequency_factor = double(SAMPLE_RATE) / fft_data.size();
-    return i * frequency_factor;
+    const size_t index_min = index_from_frequency(frequency_min, fft_data);
+    const size_t index_max = index_from_frequency(frequency_max, fft_data);
+
+    if (index_min >= fft_data.size() || index_max >= fft_data.size() || index_min > index_max)
+    {
+        throw "can't get frequencies in range!";
+    }
+
+    std::vector<FrequencyBin> in_range;
+    for(size_t i = index_min; i <= index_max; ++i)
+    {
+        in_range.push_back(fft_data[i]);
+    }
+
+    return in_range;
 }
 
 private: // methods ///////////////////////////////////////////////////////////
