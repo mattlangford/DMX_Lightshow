3ac82f108efbe420103b75a932b3f4e7a25f9ace
Matt Langford <matthew.langford95@gmail.com>
2018-01-29 23:37:17 -0500

    Add sonogram generating script

---
+++ training/README.md
@@ -0,0 +1,10 @@
+# Training data format
+I am using [this repo](https://github.com/CarlSouthall/MDBDrums) to get labeled data. The labelled data should match that. A set of timestamps and which drum is active at that timestamps, and then the drum that's active at that time. Here is a list of the possible drums:
+ - KD: kick drum
+ - SD: snare drum
+ - HH: high hat
+ - TT: tom
+ - CY: cymbol
+ - OT: other
+
+Point the script to two folder, one with training data, one with audio data (wav files). The name of the label txt file should be the same as the wav file.
---
+++ training/generate_sonograms.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import argparse
+import numpy as np
+import os
+import wave
+import pickle
+
+import cv2
+
+
+def load_audio(path):
+    # Load audio files that match the label_names
+    data = {}
+    for file_name in os.listdir(os.path.abspath(path)):
+        if file_name.endswith(".wav"):
+            data[file_name[:-4]] = wave.open(path + file_name, 'r')
+
+    return data
+
+
+def read_audio_file(audio_file):
+    audio_data_str = list(audio_file.readframes(audio_file.getnframes()))
+    audio_data = map(ord, audio_data_str)
+
+    mono = np.array(audio_data).reshape(-1, 2)
+    return np.average(mono, axis=1)
+
+
+def create_sonogram(audio_file, fft_size_samples, bin_length_seconds):
+    frames_per_bin = audio_file.getframerate() * bin_length_seconds
+    audio_data = read_audio_file(audio_file)
+
+    bins = []
+    frame_num = 0
+    data_left = len(audio_data)
+    while data_left > 0:
+        frames_processed = min(len(audio_data[frame_num:]), frames_per_bin)
+        data_left -= frames_processed
+        frame_num += int(frames_per_bin)
+
+        fft_data_complex = np.fft.fft(audio_data[frame_num - fft_size_samples:frame_num], fft_size_samples)
+        fft_data = np.absolute(fft_data_complex)[:fft_size_samples / 2]
+        bins.append(np.copy(fft_data))
+
+    sonogram = np.vstack(bins)
+    return sonogram.T
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("audio_folder", help="Folder where audio files are")
+    parser.add_argument("--show", action="store_true", default=False, help="Show the resulting sonograms")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.audio_folder):
+        raise "Not a valid path:", args.audio_folder
+
+    return args
+
+
+def main():
+    args = get_args()
+
+    audio = load_audio(args.audio_folder)
+
+    FFT_SIZE_SAMPLES = 2 ** 13 # 8k
+    BIN_LENGTH_SECONDS = 0.01 # this is as accurate as the labels
+    for file_name, audio_file in audio.iteritems():
+        print "Computing sonogram for:", file_name
+        sonogram = create_sonogram(audio_file, FFT_SIZE_SAMPLES, BIN_LENGTH_SECONDS)
+        pickle.dump(sonogram, open(args.audio_folder + file_name + ".pkl", 'w'), 2)
+
+        if args.show:
+            to_show = np.flipud(sonogram[:185, :])
+            to_show = np.clip(to_show / np.percentile(to_show, 99), 0.0, 1.0)
+            print to_show
+            cv2.imshow(file_name, cv2.resize(to_show, (0, 0), fy=1.0, fx=0.2))
+            cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
